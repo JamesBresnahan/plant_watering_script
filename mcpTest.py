5bd7d2677de388001f6873678e430f8247718ace
JamesBresnahan <jbresnahan32@gmail.com>
2020-12-12 03:47:39 +0000

    update messaging for reading

--- mcpTest.py
+++ mcpTest.py
@@ -1,14 +1,18 @@
 
 import Adafruit_MCP3008 as MCP
 
+CLK  = 18
+MISO = 23
+MOSI = 24
+CS   = 25
+mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
 def main():
-    CLK  = 18
-    MISO = 23
-    MOSI = 24
-    CS   = 25
-    mcp = MCP.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
-    x = mcp.read_adc(5)
-    print("5 reading is: ", x)
+    read_port(5)
+    
+def read_port(port):
+    current_level = mcp.read_adc(port)
+    current_reading_message = str(port) + ' has a current moisture level of ' +str(current_level)
+    print(current_reading_message)
 	
 main()
