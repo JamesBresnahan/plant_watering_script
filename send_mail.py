c63bb51ea671a892972a1c66397992d0f866af02
JamesBresnahan <jbresnahan32@gmail.com>
2020-12-12 03:34:11 +0000

    use paramerterized email messaging

--- send_mail.py
+++ send_mail.py
@@ -5,12 +5,13 @@ EMAIL_ADDRESS_SENDER= config('SENDER.EMAIL')
 EMAIL_ADDRESS_RECIPIENT= config('RECIPIENT.EMAIL')
 EMAIL_KEY= config('KEY')
 
-def send_mail():
+def send_reading_mail(plant, reading):
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
     type(smtpObj)
     smtpObj.ehlo()
     smtpObj.starttls()
     smtpObj.login(EMAIL_ADDRESS_SENDER, EMAIL_KEY)
-    smtpObj.sendmail(EMAIL_ADDRESS_SENDER, EMAIL_ADDRESS_RECIPIENT, 'Subject: This is a Test \n')
+    message_subject = 'Subject: ' + plant + ' has a current moisture level of ' + reading
+    smtpObj.sendmail(EMAIL_ADDRESS_SENDER, EMAIL_ADDRESS_RECIPIENT, message_subject)
 
-send_mail()
+send_reading_mail('dill', '111')
